b82d007d5f88589af1958f0ad1f05f941a77f91e
bruno-monteiro-1 <bruno.monteiro@vtex.com.br>
2022-05-02 15:10:50 -0300

    update setup.ts

--- node/events/setup.ts
+++ node/events/setup.ts
@@ -1,6 +1,5 @@
 import { EventContext, IOClients } from '@vtex/api'
 
 export async function setup(ctx: EventContext<IOClients>) {
-  // eslint-disable-next-line no-console
-  console.log('Evento de instalação recebido', ctx)
+  await ctx.clients.apps.installApp('vtex.list@0.x')
 }
