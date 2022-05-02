d3566f7b7ae82d52b3943d901e51ad46f89a1043
bruno-monteiro-1 <bruno.monteiro@vtex.com.br>
2022-05-02 16:03:47 -0300

    check if vtex.list is installed

--- node/events/setup.ts
+++ node/events/setup.ts
@@ -1,5 +1,23 @@
-import { EventContext, IOClients } from '@vtex/api'
+import { EventContext, IOClients, AppsListItem } from '@vtex/api'
 
-export async function setup(ctx: EventContext<IOClients>) {
-  await ctx.clients.apps.installApp('vtex.list@0.x')
+const getAllApps = (ctx: EventContext<IOClients>) => {
+  return ctx.clients.apps.listApps().then(({ data }) => data)
+}
+
+const containsApp = ({
+  list,
+  app,
+  version,
+}: {
+  app: string
+  version: number
+  list: AppsListItem[]
+}) => list.filter(item => item.id.includes(`${app}@${version}`)).length > 0
+
+export const setup = async (ctx: EventContext<IOClients>) => {
+  const apps = await getAllApps(ctx)
+
+  if (!containsApp({ list: apps, app: 'vtex.list', version: 0 })) {
+    ctx.clients.apps.installApp('vtex.list@0.x') // sem o await por não ter nenhuma ação posterior por enquanto
+  }
 }
